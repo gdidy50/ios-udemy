88a5240849d5916187b0a0f916535c7cb643d9ca
Grant Burke <gdidy50@gmail.com>
2020-06-16 23:05:14 -0500

    Added BMI Calculator

---
+++ BMICalculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by ms-devs on 6/16/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
